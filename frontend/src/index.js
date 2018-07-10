282dac5359876af655b79d271918ef3ef288240c
Andrew James <ajames20@gmail.com>
2018-07-10 08:03:29 -0600

    Add redux setup and redux devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, reduxDevTools);
+
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
