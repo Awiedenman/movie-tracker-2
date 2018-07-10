b2a21f1ab917132e7c2dcfbd9a709089555ad3d9
Andrew James <ajames20@gmail.com>
2018-07-10 07:41:35 -0600

    Add tests for api fetch call and error handling

--- frontend/src/helpers/apiCalls.js
+++ frontend/src/helpers/apiCalls.js
@@ -2,12 +2,11 @@ import { API_KEY } from '../api-key';
 
 export const movieFetch = async () => {
   const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
-  const movieData = await fetch(url);
-  const response = await movieData.json();
+  const response = await fetch(url);
 
-  if (!movieData.ok) {
+  if (!response.ok) {
     throw Error('failed fetch request');
   }
 
-  return response;
+  return await response.json();
 };
--- frontend/src/test/apiCalls.test.js
+++ frontend/src/test/apiCalls.test.js
@@ -1,7 +1,9 @@
+import { API_KEY } from '../api-key';
 import { movieFetch } from '../helpers/apiCalls';
 
 describe('HELPERS', () => {
   test('should call fetch when movieFetch is called', async () => {
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
     window.fetch = jest.fn().mockImplementation(() =>
       Promise.resolve({
         ok: true,
@@ -9,5 +11,17 @@ describe('HELPERS', () => {
       }));
     await movieFetch();
     expect(window.fetch).toHaveBeenCalled();
+    expect(window.fetch).toHaveBeenCalledWith(url);
   });
+
+  test('should throw and error if fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+        json: () => Promise.reject()
+      }));
+
+    await expect(movieFetch()).rejects.toEqual(Error('failed fetch request'));
+  });
+
 });
