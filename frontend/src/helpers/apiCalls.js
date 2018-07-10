ae1d50a66eb4f036c9f2757d2cf53f63d09a5ee5
Andrew James <ajames20@gmail.com>
2018-07-10 05:52:30 -0600

    Add test coverage for apiCalls.js

--- frontend/src/helpers/apiCalls.js
+++ frontend/src/helpers/apiCalls.js
@@ -2,7 +2,7 @@ import { API_KEY } from '../api-key';
 
 export const movieFetch = async () => {
   const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
-  const movieData = await fetch( url );
+  const movieData = await fetch(url);
   const response = await movieData.json();
   return response;
 };
