ff5b9fa8f436635cedc3a147654051032e7b11a8
Andrew James <ajames20@gmail.com>
2018-07-10 07:08:35 -0600

    Add error handling for movieFetch apiCalls.js

--- frontend/src/helpers/apiCalls.js
+++ frontend/src/helpers/apiCalls.js
@@ -4,5 +4,10 @@ export const movieFetch = async () => {
   const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
   const movieData = await fetch(url);
   const response = await movieData.json();
+
+  if (!movieData.ok) {
+    throw Error('failed fetch request');
+  }
+
   return response;
 };
