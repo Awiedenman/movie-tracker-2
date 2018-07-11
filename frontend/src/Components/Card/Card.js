ae353c35c3dd0f496ea0c9f9208bb2d981630916
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-11 10:01:05 -0600

    Render movies on page

---
+++ frontend/src/Components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Card = ({ average, id, title, image, overview }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      
+
+    </div>
+
+  );
+};
--- frontend/src/Containers/Home/Home.js
+++ frontend/src/Containers/Home/Home.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { movieFetch } from '../../helpers/apiCalls';
 import { cleanMovieResponse } from '../../helpers/clean-responses';
 import { fetchInitialMovies } from '../../Actions';
+import { Card } from '../../Components/Card/Card';
 import PropTypes from 'prop-types';
 
 export class Home extends Component {
@@ -10,16 +11,26 @@ export class Home extends Component {
     super();
   }
 
+  
   async componentDidMount(){
     const initialFetch = await movieFetch();
     const movies = cleanMovieResponse(initialFetch);
     this.props.initialFetchData(movies);
   }
-
+  
   render() {
+    // console.log(this.props)
+    const displayMoviesCards = this.props.movies.initialMovies.map(movie => (
+      <Card 
+        {...movie}
+        key={movie.id}
+      />
+    ));
+
     return (
       <div>
         <h1>home</h1>
+        {displayMoviesCards}
       </div>
     );
   }
@@ -34,7 +45,8 @@ export const mapStateToProps = movies => ({
 });
 
 Home.propTypes = {
-  initialFetchData: PropTypes.func
+  initialFetchData: PropTypes.func,
+  movies: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
