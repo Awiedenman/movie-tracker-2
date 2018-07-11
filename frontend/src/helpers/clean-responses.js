20cf09202a551ab30b769829bc19ba747f07631c
Andrew James <ajames20@gmail.com>
2018-07-11 09:26:36 -0600

    Update data cleaner

--- frontend/src/helpers/clean-responses.js
+++ frontend/src/helpers/clean-responses.js
@@ -1,1 +1,7 @@
-export const cleanMovieResponse = response => (response.items);
+export const cleanMovieResponse = response => response.items.map(item => ({
+  average: item.vote_average,
+  id: item.id,
+  title: item.title,
+  image: item.poster_path,
+  overview: item.overview
+}));
--- frontend/src/mock-data/mock-clean-data.js
+++ frontend/src/mock-data/mock-clean-data.js
@@ -1,25 +1,9 @@
-export const mockCleanedMovieList = [
+export const mockCleanedMovieList =[
   {
-    "vote_average": 7.5,
-    "vote_count": 7179,
-    "id": 284053,
-    "video": false,
-    "media_type": "movie",
-    "title": "Thor: Ragnarok",
-    "popularity": 150.406,
-    "poster_path": "/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
-    "original_language": "en",
-    "original_title": "Thor: Ragnarok",
-    "genre_ids": [
-      28,
-      12,
-      14,
-      878,
-      35
-    ],
-    "backdrop_path": "/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg",
-    "adult": false,
-    "overview": "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.",
-    "release_date": "2017-10-25"
+    average: 7.5,
+    id: 284053,
+    image: "/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
+    overview: "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.",
+    title: "Thor: Ragnarok"
   }
 ];
