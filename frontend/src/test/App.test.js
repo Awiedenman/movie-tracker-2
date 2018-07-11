94e1f66708f462b61e3f64e036d980c34947902a
Andrew James <ajames20@gmail.com>
2018-07-11 08:26:41 -0600

    Major refactor move mstp mdtp from app in to Home

--- frontend/src/test/App.test.js
+++ frontend/src/test/App.test.js
@@ -1,50 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { App, mapDispatchToProps, mapStateToProps } from '../Containers/App/App';
-import { fetchInitialMovies } from '../Actions';
-import { mockInitialMovieResponse } from '../mock-data/mock-responses';
-import { mockCleanedMovieList } from '../mock-data/mock-clean-data';
+import App from '../Components/App/App';
 
 describe('App', () => {
   let wrapper;
-  const mockFunc = jest.fn();
-  beforeEach(() => wrapper = shallow(<App initialFetchData={mockFunc}/>));
+  beforeEach(() => wrapper = shallow(<App />));
 
 
   describe('App', () => {
-    test('should ', async () => {
-      await wrapper.instance().componentDidMount();
-      expect(mockFunc).toHaveBeenCalled();
-    });
-
     test('renders without crashing', () => {
       expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-
-  describe('mapDispatchToProps', () => {
-    test('should call dispatch when MDTP is called', () => {
-      const mockDispatch = jest.fn();
-      const actionToDipatch = fetchInitialMovies(mockInitialMovieResponse);
-      const mappedProps = mapDispatchToProps(mockDispatch);
-
-      mappedProps.initialFetchData(mockInitialMovieResponse);
-
-      expect(mockDispatch).toHaveBeenCalledWith(actionToDipatch);
-
-    });
-  });
-
-  describe('mapStateToProps', () => {
-    test('should return a props object with a movies array', () => {
-      const expected = {
-        movies: mockCleanedMovieList
-      };
-
-      const mockMappedState = mapStateToProps(mockCleanedMovieList);
-
-      expect(mockMappedState).toEqual(expected);
     });
   });
 });
---
+++ frontend/src/test/Home.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Home, mapDispatchToProps, mapStateToProps } from '../Containers/Home';
+import { fetchInitialMovies } from '../Actions';
+import { mockInitialMovieResponse } from '../mock-data/mock-responses';
+import { mockCleanedMovieList } from '../mock-data/mock-clean-data';
+
+describe('Home', () => {
+  let wrapper;
+  const mockFunc = jest.fn();
+  beforeEach(() => wrapper = shallow(<Home initialFetchData={mockFunc}/>));
+
+
+  describe('Home', () => {
+    test('should ', async () => {
+      await wrapper.instance().componentDidMount();
+      expect(mockFunc).toHaveBeenCalled();
+    });
+
+    test('renders without crashing', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+
+  describe('mapDispatchToProps', () => {
+    test('should call dispatch when MDTP is called', () => {
+      const mockDispatch = jest.fn();
+      const actionToDipatch = fetchInitialMovies(mockInitialMovieResponse);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.initialFetchData(mockInitialMovieResponse);
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDipatch);
+
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    test('should return a props object with a movies array', () => {
+      const expected = {
+        movies: mockCleanedMovieList
+      };
+
+      const mockMappedState = mapStateToProps(mockCleanedMovieList);
+
+      expect(mockMappedState).toEqual(expected);
+    });
+  });
+});
+
+
