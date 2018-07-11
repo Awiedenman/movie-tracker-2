c3c166ef1d7c6fb2ab2a57621bf63ae4f5439df3
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-11 10:10:13 -0600

    Render movie posters on the page

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 export const Card = ({ average, id, title, image, overview }) => {
   return (
-    <div>
+    <div id={id}>
+      <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${image}`} alt={`Movie poster for ${title}`} />
       <h2>{title}</h2>
-      
-
+      <h4>Viewer Average: {average}</h4>
+      <p>{overview}</p>
     </div>
 
   );
