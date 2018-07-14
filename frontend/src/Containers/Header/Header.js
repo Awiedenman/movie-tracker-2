c1de238f6b0e270347d57b0757686fb40dee63e2
Andrew James <ajames20@gmail.com>
2018-07-14 13:16:19 -0600

    Logout Complete with test coverage

--- frontend/src/Containers/Header/Header.js
+++ frontend/src/Containers/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { userSignOut } from '../../Actions/index';
 import PropTypes from 'prop-types';
 
 import './Header.css';
 
 export const Header = props => {
-  const { user } = props;
-
+  const { user, signOut } = props;
+  // console.log(props);
   return (
     <header className="header">
       <NavLink exact to="/">
@@ -15,19 +16,31 @@ export const Header = props => {
       </NavLink>
       <nav>
         {user.name && <p>Welcome back {user.name}</p>}
-        <NavLink exact to="/login">Sign In</NavLink>
-        <NavLink to="/sign-up">Sign Up</NavLink>
+        {
+          user.name ?
+            <button className="sign-out" onClick={() => signOut()}>Sign Out</button>
+            :
+            <div>
+              <NavLink exact className="sign-in" to="/login">Sign In</NavLink>
+              <NavLink exact to="/sign-up">Sign Up</NavLink>
+            </div>
+        }
       </nav>
     </header>
   );
 };
 
 Header.propTypes = {
-  user: PropTypes.object
+  user: PropTypes.object,
+  signOut: PropTypes.func
 };
 
 export const mapStateToProps = state => ({
   user: state.userInfo
 });
 
-export default connect(mapStateToProps)(Header);
+export const mapDispatchToProps = dispatch => ({
+  signOut: () => dispatch(userSignOut())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- frontend/src/test/Header.test.js
+++ frontend/src/test/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Header, mapStateToProps } from '../Containers/Header/Header';
+import { Header, mapStateToProps, mapDispatchToProps } from '../Containers/Header/Header';
+import { userSignOut } from '../Actions';
 
 describe('Header', () => {
   let wrapper;
@@ -9,7 +10,21 @@ describe('Header', () => {
     name: 'Bob'
   };
 
-  beforeEach(() => wrapper = shallow(<Header user={user} />));
+  const mockFunc = jest.fn();
+
+  beforeEach(() => wrapper = shallow(
+    <Header user={user}
+      signOut={mockFunc}
+    />
+  ));
+
+  describe('user sign out',  () => {
+    test('should sign user out on click', () => {
+      wrapper.find('.sign-out').simulate('click');
+
+      expect(mockFunc).toHaveBeenCalled();
+    });
+  });
 
   describe('mapStateToProps', () => {
     test('should return a user object with user information', () => {
@@ -30,11 +45,28 @@ describe('Header', () => {
       const actual = mapStateToProps(mockState);
       expect(actual).toEqual(mockUser);
     });
+  });
 
+  describe('mapDispatchToProps', () => {
+    test('should call dispatch with the action signOut', () => {
+      const mockDispatch = jest.fn();
+      const mockActionToDispatch = userSignOut();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.signOut();
+
+      expect(mockDispatch).toHaveBeenCalledWith(mockActionToDispatch);
+    });
   });
 
 
-  test('should match snapshot', () => {
+  test('should match when logged out', () => {
+    wrapper = shallow(<Header user={{}} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should match snapshot logged in', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
