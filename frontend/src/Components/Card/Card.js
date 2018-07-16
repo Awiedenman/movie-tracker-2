a006202c6e6cc3657fbc20faec6f6a7d622f897e
Andrew James <ajames20@gmail.com>
2018-07-16 08:20:53 -0600

    Add basic styles for hover on card

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Register from '../Register/Register';
 
 import './Card.css';
 
-export const Card = props => {
+const Card = props => {
   const { average, id, title, image, overview, toggleFavorite, userId } = props;
 
   return (
-    <div  className='card' id={id}>
-      <Route render={({ history }) => (
-        <button
-          onClick={userId ? () => toggleFavorite(id) : () => { history.push('/sign-up'); }}
-        >
-      ♡
-        </button>
-      )}
-      />
-      <img className='card__image'
+    <div  className="card" id={id}>
+      <img className="card__image"
         src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${image}`}
         alt={`Movie poster for ${title}`}
       />
-      <h2 className='card__title'>{title}</h2>
-      <h4 className='card__avg'>Viewer Average: {average}</h4>
-      <p className='card__overview' >{overview}</p>
+      <h2 className="card__title card__description">{title}</h2>
+      <h4 className="card__avg card__description">Viewer Average: {average}</h4>
+      <p className="card__overview card__description" >{overview}</p>
+      {userId && <button className=""onClick={() => toggleFavorite(id)}>♡</button>}
+      {!userId && <Register />}
     </div>
   );
 };
---
+++ frontend/src/Components/Register/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () =>  (
+  <Link to="/sign-up" >♡ Sign In/Sign Up to Favorite</Link>
+);
+
+export default Register;
